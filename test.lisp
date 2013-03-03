4b996ee3bd49980dc020046865ab9608139a8a86
sjkyspa <sjkyspa@gmail.com>
2013-03-03 22:57:32 +0800

    到牛顿求平方

---
+++ test.lisp
@@ -0,0 +1,31 @@
+(define (big x y) (if (> x y) x y))
+(define (max a b c) (if (= (big a b) (big b c)) (+ b (big a c)) (+ (big a b) (big b c))))
+
+(define (a-plus-abs-b a b)
+    ((if (> b 0) + -) a b))
+(define (test a b) (+ a b) (- a b))
+ 
+(define (p) (p))
+
+(define (test x y)
+    (if ( = x 0)
+        0
+        y))
+
+(define (sqrt-iter guess x)
+    (if (good-enough? guess x)
+        guess
+        (sqrt-iter (improve guess x)
+                    x)))
+
+(define (good-enough? guess x)
+    (< (abs (- (square guess) x)) 0.0001))
+
+(define (improve guess x)
+    (average guess (/ x guess)))
+
+(define (average x y)
+    (/ (+ x y) 2))
+
+(define (sqrt a)
+    (sqrt-iter 1.0 a))
